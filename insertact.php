50d002085c3285089ef14560bdda652384e6868c
Jay-ar Dela Rama <jayardelarama27@gmail.com>
2023-05-20 20:01:42 +0800

    added due date and link location

--- insertact.php
+++ insertact.php
@@ -8,17 +8,25 @@
         $actcode = $_POST['act-code'];
         // $teacher = $_POST[''];
         $actscore = $_POST['act-score'];
+        $actdue = $_POST['act-due'];
+        $dateTime = new DateTime($actdue);
+        $formattedDueDate = $dateTime->format('Y-m-d H:i:s');
 
-        $sql = "INSERT INTO tbl_codeact (TITLE, ACT_DESC, OUTPUT, S_CODE, TEACHER, SCORE, DATE_CREATED)VALUES ('$acttitle', '$actdesc', '$outputcode', '$actcode', 'DELARAMA', $actscore, NOW())" ;
+        $sql = "INSERT INTO tbl_codeact (TITLE, ACT_DESC, OUTPUT, S_CODE, TEACHER, SCORE, DATE_CREATED, DUE_DATE) VALUES ('$acttitle', '$actdesc', '$outputcode', '$actcode', 'DELARAMA', $actscore, NOW(), '$formattedDueDate')";
+        //('$acttitle', '$actdesc', 'sgfsdfgs', 'adfasdf', 'DELARAMA', $actscore, NOW(), '$formattedDueDate')" ;
+        // ('$acttitle', '$actdesc', '$outputcode', '$actcode', 'DELARAMA', $actscore, NOW()), , '$formattedDueDate'" ;
         $result = $conn -> query($sql);
 
         if ($result) {
-            echo "Data inserted successfully.";
+           // echo "Data inserted successfully.";
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
 
         mysqli_close($conn);
+
+        header("Location: coddie-instructor-platform.php");
+        exit;
     }
 
 ?>
